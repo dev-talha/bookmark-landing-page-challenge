d8113a5e7e1dfc5bd41bde4df8bd79a9eda4f633
mdriaz <riazmd582@gmail.com>
2020-07-06 00:51:29 +0600

    60% completed for today

---
+++ js/main.js
@@ -0,0 +1,48 @@
+"use strict";
+
+// Declare all variables
+const tabcontent = document.querySelectorAll(".tabcontent");
+const tablinks = document.querySelectorAll(".tablinks");
+const accordion = document.querySelectorAll(".accordion_title");
+
+window.onload = () => {
+  document.getElementById("defaultOpen").click();
+
+  /*---------------
+    Accordion Action
+  -----------------*/
+  Array.from(accordion).forEach((acc) => {
+    acc.addEventListener("click", function () {
+      /* Toggle between adding and removing the "active" class,
+      to highlight the button that controls the panel */
+      this.classList.toggle("active");
+
+      /* Toggle between hiding and showing the active panel */
+      let panel = this.nextElementSibling;
+      if (panel.style.maxHeight) {
+        panel.style.maxHeight = null;
+      } else {
+        panel.style.maxHeight = panel.scrollHeight + "px";
+      }
+    });
+  });
+};
+
+/*-----------------
+  Tabs Action
+  ----------------*/
+
+let openTab = (evt, tabId) => {
+  //   Get all elements with class="tabcontent" and hide them
+  Array.from(tabcontent).forEach((tab) => {
+    tab.style.display = "none";
+  });
+  // Get all elements with class="tablinks" and remove the class "active"
+  Array.from(tablinks).forEach((tlink) => {
+    tlink.classList.remove("active");
+  });
+  // Show the current tab, and add an "active" class to the button that opened the tab
+  Array.from(tablinks).forEach;
+  document.getElementById(tabId).style.display = "flex";
+  evt.currentTarget.className += " active";
+};
